d6f174a74dc8d978e8f2c1350273064ef6b10346
stapleriy <simran.poudel3@gmail.com>
2024-10-21 11:24:08 +0545

    refactor

---
+++ All/working.js
@@ -0,0 +1,12 @@
+const menuIcon = document.getElementById("menuIcon");
+const navLinks = document.getElementById("navLinks");
+
+menuIcon.addEventListener("click", function() {
+    navLinks.classList.toggle("active"); // Toggle the active class
+});
+const menuIcon = document.getElementById("menuIcon");
+const navLinks = document.getElementById("navLinks");
+
+menuIcon.addEventListener("click", function() {
+    navLinks.classList.toggle("active"); // Toggle the active class
+});
